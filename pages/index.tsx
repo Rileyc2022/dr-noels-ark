7b066e4831de423a233db313ba7c7a4a1cb0f9a7
Rileyc2022 <rileyc2022@headroyce.org>
2022-02-20 21:15:50 -0800

    Testing favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,24 @@ const Home: NextPage = () => {
             <Head>
                 <title>Dr. Noel's Ark</title>
                 <meta name="description" content="Dr. Noel's Ark" />
-                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-16.png"
+                    sizes="16x16"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-32.png"
+                    sizes="32x32"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-2100.png"
+                    sizes="2100x2100"
+                />
             </Head>
             <Box
                 height={"100vh"}
